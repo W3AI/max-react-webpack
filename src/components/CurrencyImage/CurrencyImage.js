9b96d98f916c2dbe8ede008715d7c747be52c0a8
stefian <v_ianta@yahoo.com>
2019-05-28 18:44:23 -0400

    added CurrencyImage styles

--- src/components/CurrencyImage/CurrencyImage.js
+++ src/components/CurrencyImage/CurrencyImage.js
@@ -1,9 +1,12 @@
 import React from 'react;'
 
 import classes from './CurrencyImage.css';
+import CurrencyImage from '../../assets/currency.jpg';
 
 const currencyImage = (props) => (
-    <div className={classes.CurrencyImage}></div>
+    <div className={classes.CurrencyImage}>
+        <img src={CurrencyImage} className={classes.CurrencyImg} />
+    </div>
 );
 
 export default currencyImage;
