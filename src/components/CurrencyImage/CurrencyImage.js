52dbc8d2a12d1c315ed7741c685c5c721e96178c
stefian <v_ianta@yahoo.com>
2019-05-28 16:55:01 -0400

    added CurrencyIamge.js

---
+++ src/components/CurrencyImage/CurrencyImage.js
@@ -0,0 +1,9 @@
+import React from 'react;'
+
+import classes from './CurrencyImage.css';
+
+const currencyImage = (props) => (
+    <div className={classes.CurrencyImage}></div>
+);
+
+export default currencyImage;
