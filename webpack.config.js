44bc77600695b6adfd510351c245d1f88c20b25b
stefian <v_ianta@yahoo.com>
2019-05-29 10:32:48 -0400

    finalized css config in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
@@ -26,6 +27,7 @@ module.exports = {
                     { 
                         loader: 'css-loader',
                         options: {
+                            importLoaders: 1,
                             modules: true,
                             localIdentName: '[name]__[local]__[hash:base64:5]'
                         }
@@ -35,7 +37,12 @@ module.exports = {
                         options: {
                             ident: 'postcss',
                             plugins: () => [
-                                
+                                autoprefixer({
+                                    browsers: [
+                                        "> 1%",
+                                        "last 2 versions"
+                                    ]
+                                })
                             ]
                         }
                      }
